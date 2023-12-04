752aea598bb75223c85e9e1e15536e09b03040de
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2023-12-05 00:54:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const cors = require('cors')
+
+const mongoose = require('mongoose')
+
+const userRoutes = require('./routes/userRoutes')
+
+const app = express()
+
+require('dotenv').config();
+
+app.use(cors())
+app.use(express.json())
+
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true, useUnifiedTopology: true 
+})
+.then(()=>{
+    console.log('DB connection Successful')
+}).catch((err)=>{
+    console.log(err.message);
+})
+
+app.use('/api/auth',userRoutes)
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server Started on PORT ${process.env.PORT}`)
+})
